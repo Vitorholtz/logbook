ee44d7b7a3b520545e1916aa70291e77219a4ad3
Vítor Holtz <vitorholtz27@gmail.com>
2020-12-10 11:20:14 -0300

    menu

---
+++ logbook_script.js
@@ -0,0 +1,13 @@
+
+const navSlide = () => {
+
+   const burger = document.querySelector ('.burger');
+   const nav = document.querySelector ('.nav-list');
+
+   burger.addEventListener('click', () => {
+       nav.classList.toggle('nav-list-active');
+   });
+
+}
+
+   navSlide ();
